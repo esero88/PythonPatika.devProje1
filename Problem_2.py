ad693d8ac55001514d4b3d7eb5458e381694e9da
Eser Karaceper <52802861+esero88@users.noreply.github.com>
2021-08-16 19:08:36 +0300

    Update Problem_2.py
    Notlarda güncellemeler yapıldı, daha açıklayıcı hale getirildi

--- Problem_2.py
+++ Problem_2.py
@@ -4,10 +4,10 @@ def reverselist(x):
 
     for i in range(l):
         j = -i-1    #Negatif sayılar için
-        if type(x[j]) is list:  #Liste mi diye kontrol
-            result.append(reverselist(x[j]))    #Eğer öyle ise yeniden döndür
+        if type(x[j]) is list:  #Liste içinde liste var mı diye kontrol
+            result.append(reverselist(x[j]))    #Eğer öyle ise reverselist(x) için alt listeyi döndür ve result listesine ekle
         else:
-            result.append(x[j])     #Aksi halde listenin sonuna ekle
+            result.append(x[j])     #Aksi halde result listesinin sonuna ekle
     return result
 
 x = [[1, 2], [3, 4], [5, 6, 7]]
